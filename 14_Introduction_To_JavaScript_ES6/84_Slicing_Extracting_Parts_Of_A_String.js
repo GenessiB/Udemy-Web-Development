6a12b0c013ed1e0bac28a8b04f7933ad0c28bab0
Genessi Barot <genessibarot@gmail.com>
2024-05-08 19:04:44 -0400

    .

--- 14_Introduction_To_JavaScript_ES6/84_Slicing_Extracting_Parts_Of_A_String.js
+++ 14_Introduction_To_JavaScript_ES6/84_Slicing_Extracting_Parts_Of_A_String.js
@@ -1,7 +1,14 @@
 /** Slicing and Extracting Parts of a String
  ** Specifies which slice of information you want from a string
  ** .slice(Y,X) (start, end)
+ Capital G is at position 0, letter i is at position 6
+ * a b c
+ * 0 1 2
+ * slice(0,1); = a // upto but not including 1
+ * slice(1,3); = a b // upto but not including 3
  */
 
 var myName = "Genessi";
-myName.slice(0,1)
+myName.slice(0,1); // would output "G"
+myName.slice(6,7); // would output "i"
+myName.slice(0,3); // would output "Gen"
