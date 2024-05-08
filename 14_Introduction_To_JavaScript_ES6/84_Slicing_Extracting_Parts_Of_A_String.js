76cf1ce7cc5d00953de46a5ea961b4d7c152a102
Genessi Barot <genessibarot@gmail.com>
2024-05-08 18:40:14 -0400

    notes for slicing

---
+++ 14_Introduction_To_JavaScript_ES6/84_Slicing_Extracting_Parts_Of_A_String.js
@@ -0,0 +1,7 @@
+/** Slicing and Extracting Parts of a String
+ ** Specifies which slice of information you want from a string
+ ** .slice(Y,X) (start, end)
+ */
+
+var myName = "Genessi";
+myName.slice(0,1)
